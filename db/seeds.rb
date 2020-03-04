331155a2795370c2851711759f59205cfea41bfc
Dan Clair <danclair@me.com>
2020-03-03 18:53:15 -0600

    Add example products to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+my_product = Product.new({ name: "Hoodie", price: 19, description: "Soft knit zip-front hoodie with pullstrings", category: "Outerwear" })
+my_product.save
+
+my_product2 = Product.new({ name: "Ankle Socks", price: 12, description: "Moisture wicking knit socks", category: "Footwear" })
+my_product2.save
+
+my_product3 = Product.new({ name: "Slim-fit Khakis", price: 24, description: "Slim fit woven pants, 85% cotton, 15% lycra", category: "Pants" })
+my_product3.save
