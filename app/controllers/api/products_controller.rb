aba208604e287725fce2d96bbc916973cd1e4274
Dan Clair <danclair@me.com>
2020-03-03 19:08:52 -0600

    Add controller actions and view files for multiple products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,21 @@
 class Api::ProductsController < ApplicationController
   def all_products_action
+    @all_products = Product.all
     render "all_products.json.jb"
+  end
+
+  def one_hoodie_action
+    @one_hoodie = Product.find_by(id: 1)
+    render "one_hoodie.json.jb"
+  end
+
+  def one_sock_action
+    @one_sock = Product.find_by(id: 2)
+    render "one_sock.json.jb"
+  end
+
+  def one_pants_action
+    @one_pants = Product.find_by(id: 3)
+    render "one_pants.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products" => "products#all_products_action"
+    get "/one_hoodie" => "products#one_hoodie_action"
+    get "/one_sock" => "products#one_sock_action"
+    get "/one_pants" => "products#one_pants_action"
   end
 end
