c53e4b765401c6bd98b9c29feaafae54ed130e9a
Dan Clair <danclair@me.com>
2020-03-12 18:41:14 -0500

    Remove image_url column from product table

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,29 @@
 #
 # Examples:
 #
-product = Product.new({ name: "Hoodie", price: 19, description: "Soft knit zip-front hoodie with pullstrings", image_url: "https://oldnavy.gap.com/webcontent/0010/698/786/cn10698786.jpg", category: "Outerwear" })
+supplier = Supplier.new({ name: "Old Navy", email: "info@oldnavy.com", phone_number: "312-555-5555" })
+supplier.save
+
+supplier2 = Supplier.new({ name: "New Balance", email: "info@newbalance.com", phone_number: "312-444-4444" })
+supplier2.save
+
+supplier3 = Supplier.new({ name: "Apple", email: "info@apple.com", phone_number: "312-111-1111" })
+supplier3.save
+
+product = Product.new({ name: "Hoodie", price: 19, description: "Soft knit zip-front hoodie with pullstrings", image_url: "https://oldnavy.gap.com/webcontent/0010/698/786/cn10698786.jpg" })
 product.save
 
-product2 = Product.new({ name: "Ankle Socks", price: 12, description: "Moisture wicking knit socks", image_url: "https://images-na.ssl-images-amazon.com/images/I/710qXy1vL4L._AC_UX679_.jpg", category: "Footwear" })
+product2 = Product.new({ name: "Ankle Socks", price: 12, description: "Moisture wicking knit socks", image_url: "https://images-na.ssl-images-amazon.com/images/I/710qXy1vL4L._AC_UX679_.jpg" })
 product2.save
 
-product3 = Product.new({ name: "Slim-fit Khakis", price: 24, description: "Slim fit woven pants, 85% cotton, 15% lycra", image_url: "https://i.pinimg.com/originals/fb/9e/66/fb9e66a911b869ac03de2807ce39b125.jpg", category: "Pants" })
+product3 = Product.new({ name: "Slim-fit Khakis", price: 24, description: "Slim fit woven pants, 85% cotton, 15% lycra", image_url: "https://i.pinimg.com/originals/fb/9e/66/fb9e66a911b869ac03de2807ce39b125.jpg" })
 product3.save
+
+product4 = Product.new({ name: "Cool Widget", price: 30, description: "Widget that does cool things", image_url: "https://support.apple.com/library/content/dam/edam/applecare/images/en_US/social/ios13-iphone-xs-widget-social-card.jpg" })
+product4.save
+
+product5 = Product.new({ name: "New Thing", price: 15, description: "This thing is so new", image_url: "https://i.ytimg.com/vi/2zXtIQzK8Xc/maxresdefault.jpg" })
+product5.save
+
+product6 = Product.new({ name: "Another New Thing", price: 99, description: "This thing is even newer", image_url: "https://i.ytimg.com/vi/boSfiF-ItwI/maxresdefault.jpg" })
+product6.save
