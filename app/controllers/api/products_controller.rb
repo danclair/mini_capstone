645a6db3d35e1852de173bf9751c4a4061e7ee20
Dan Clair <danclair@me.com>
2020-03-16 19:34:53 -0500

    Add users and authentication, and orders and associations

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -10,10 +10,6 @@ class Api::ProductsController < ApplicationController
       @products = @products.where("price < 15")
     end
 
-    # if params[:sort] == "price" && params[:sort_order] == "asc"
-    #   @products = @products.order(price: :asc)
-    # elsif params[:sort] == "price" && params[:sort_order] == "desc"
-    #   @products = @products.order(price: :desc)
     if params[:sort] && params[:sort_order]
       @products = @products.order(params[:sort] => params[:sort_order])
     else
@@ -27,10 +23,11 @@ class Api::ProductsController < ApplicationController
     @product = Product.new(
       name: params[:name],
       price: params[:price],
-      image_url: params[:image_url],
       description: params[:description],
+      supplier_id: params[:supplier_id],
     )
     if @product.save
+      Image.create(url: params["image_url"], product_id: @product.id)
       render "show.json.jb"
     else
       render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
@@ -46,9 +43,7 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     @product.name = params[:name] || @product.name
     @product.price = params[:price] || @product.price
-    @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
-    @product.category = params[:category] || @product.category
     if @product.save
       render "show.json.jb"
     else
@@ -61,26 +56,4 @@ class Api::ProductsController < ApplicationController
     product.destroy
     render json: { message: "Product successfully destroyed" }
   end
-
-  # def all_products_action
-  #   @all_products = Product.all
-  #   render "all_products.json.jb"
-  # end
-
-  # def all_product_names_action
-  #   @all_products = Product.all
-  #   @all_product_names = []
-  #   index = 0
-  #   while index < @all_products.length
-  #     @all_product_names << @all_products[index][:name]
-  #     index += 1
-  #   end
-  #   render "all_product_names.json.jb"
-  # end
-
-  # def any_product_action
-  #   input_product = params["product_id"]
-  #   @any_product = Product.find_by(id: input_product)
-  #   render "any_product.json.jb"
-  # end
 end
